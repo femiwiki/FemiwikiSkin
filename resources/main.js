4b6c93f11a9d700ac9468086ebff7cf436c99c72
lens0021 <lorentz0021@gmail.com>
2018-10-16 20:15:19 +0900

    다면분류 js 제거
    femiwiki/skin#51

--- resources/main.js
+++ resources/main.js
@@ -2,7 +2,6 @@ var _FW = {
 	BBS_NS: [3902, 3904]
 };
 
-
 $( function () {
 	function menuResize( divId ) {
 		var containerWidth = parseFloat( $( '#' + divId ).css( 'width' ) )
@@ -58,35 +57,6 @@ $( function () {
 		$( '#fw-menu-toggle .badge' )
 			.addClass( 'active' )
 			.text( badge > 10 ? '+9' : badge )
-	}
-
-	// Collapsible category links
-	var catlinksToggle = $( '<button></button>' );
-	catlinksToggle.text( '►' );
-	catlinksToggle.addClass( 'fw-catlinks-toggle' );
-
-	var catlinks = $( '#mw-normal-catlinks li' ),
-		directCatAnchors = $( '#fw-catlinks li > a' ),
-		directCatTexts = {};
-	for ( var i=0, len=directCatAnchors.length ; i < len ; i++ )
-		directCatTexts[directCatAnchors[i].text] = true;
-
-	if ( directCatAnchors.length !== catlinks.length ) {
-		for ( var i = 0, len = catlinks.length ; i < len ; i++ )
-			if ( !directCatTexts[catlinks[i].innerText] )
-			 catlinks[i].className += ' collapsible' ;
-
-		$( '#catlinks li.collapsible' ).fadeOut();
-		var collapsed = true;
-		catlinksToggle.click( function () {
-			$( this ).text( $( this ).text() == "▼" ? "►" : "▼" );
-			if ( collapsed )
-				$( '#catlinks li.collapsible' ).fadeIn();
-			else
-				$( '#catlinks li.collapsible' ).fadeOut();
-			collapsed = !collapsed;
-		} );
-		$( '#mw-normal-catlinks' ).prepend( catlinksToggle );
 	}
 
 	// Set Mathjax linebreaks configuration
