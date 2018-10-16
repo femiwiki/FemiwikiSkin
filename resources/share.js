df0776ff846b667b9ac71d44586a07bfbab747cb
lens0021 <lorentz0021@gmail.com>
2018-10-16 13:51:28 +0900

    config 값이 없을 때 처리 추가

--- resources/share.js
+++ resources/share.js
@@ -48,32 +48,36 @@
 			$editorEl.hide();
 		} );
 
-		window.fbAsyncInit = function() {
-			FB.init( {
-				appId						: facebookAppId,
-				autoLogAppEvents : true,
-				xfbml						: true,
-				version					: 'v2.10'
+		if ( facebookAppId == '' ) {
+			$( '#share-facebook' ).remove();
+		} else {
+			window.fbAsyncInit = function() {
+				FB.init( {
+					appId            : facebookAppId,
+					autoLogAppEvents : true,
+					xfbml            : true,
+					version          : 'v2.10'
+				} );
+				FB.AppEvents.logPageView();
+			};
+
+			(function(d, s, id){
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) {return;}
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/en_US/sdk.js";
+				fjs.parentNode.insertBefore(js, fjs);
+			})( document, 'script', 'facebook-jssdk' );
+
+			$( '#share-facebook a' ).on( 'click', function( e ) {
+				e.preventDefault();
+
+				FB.ui( {
+					method: 'share',
+					href: longURL,
+				}, function( response ) { } );
 			} );
-			FB.AppEvents.logPageView();
-		};
-
-		(function(d, s, id){
-			 var js, fjs = d.getElementsByTagName(s)[0];
-			 if (d.getElementById(id)) {return;}
-			 js = d.createElement(s); js.id = id;
-			 js.src = "//connect.facebook.net/en_US/sdk.js";
-			 fjs.parentNode.insertBefore(js, fjs);
-		 })( document, 'script', 'facebook-jssdk' );
-
-		$( '#share-facebook a' ).on( 'click', function( e ) {
-			e.preventDefault();
-
-			FB.ui( {
-			method: 'share',
-			href: longURL,
-		}, function( response ) { } );
-		} );
+		}
 	}
 
 	function updateURL( url ) {
@@ -86,6 +90,9 @@
 
 	function createShortURL( longURL ) {
 		shortURL = undefined;
+
+		if ( firebaseKey == '' )
+			return;
 
 		var params = {
 			"dynamicLinkInfo": {
