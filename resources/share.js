7e700bed4c57959f47266762ee9661f8a39d7f2a
lens0021 <lorentz0021@gmail.com>
2018-10-16 12:12:59 +0900

    MediaWiki JavaScript Coding conventions
    https://www.mediawiki.org/wiki/Manual:Coding_conventions/JavaScript

--- resources/share.js
+++ resources/share.js
@@ -1,128 +1,127 @@
-(function() {
-  const firebaseKey = 'AIzaSyCmc0RONpZQLy22clpSsE7jVbGmKQ8pZC8';
-  const facebookAppId = '1937597133150935';
-  var longURL, shortURL;
+( function() {
+	const firebaseKey = 'AIzaSyCmc0RONpZQLy22clpSsE7jVbGmKQ8pZC8';
+	const facebookAppId = '1937597133150935';
+	var longURL, shortURL;
 
-  var $editorEl = $(
-    '<div id="share" class="dialog" style="display: none;">' +
-    '<ul class="share-list">' +
-    '<li id="share-facebook"><a href="#"><span>페이스북</span></a></li>' +
-    '<li id="share-twitter"><a href="#"><span>트위터</span></a></li>' +
-    '</ul>' +
-    '<form>' +
-    '  <textarea readonly id="share-text"></textarea>' +
-    '</form>' +
-    '<form>' +
-    '  <input type="reset" value="닫기">' +
-    '</form>' +
-    '</div>'
-  );
+	var $editorEl = $(
+		'<div id="share" class="dialog" style="display: none;">' +
+		'<ul class="share-list">' +
+		'<li id="share-facebook"><a href="#"><span>페이스북</span></a></li>' +
+		'<li id="share-twitter"><a href="#"><span>트위터</span></a></li>' +
+		'</ul>' +
+		'<form>' +
+		'	<textarea readonly id="share-text"></textarea>' +
+		'</form>' +
+		'<form>' +
+		'	<input type="reset" value="닫기">' +
+		'</form>' +
+		'</div>'
+	);
 
-  function _initUI() {
-    longURL = window.location.href;
+	function _initUI() {
+		longURL = window.location.href;
 
-    $(document.body).append($editorEl);
-    $('#p-share a').on('click', function(e) {
-      e.preventDefault();
+		$( document.body ).append( $editorEl );
+		$( '#p-share a' ).on( 'click', function( e ) {
+			e.preventDefault();
 
-      if ( longURL != window.location.href ) {
-        longURL = window.location.href;
-        shortURL = undefined;
-        updateURL( longURL );
-      }
+			if ( longURL != window.location.href ) {
+				longURL = window.location.href;
+				shortURL = undefined;
+				updateURL( longURL );
+			}
 
-      $editorEl.show();
+			$editorEl.show();
 
-      updateURL( shortURL!==undefined?shortURL:longURL );
-      $('textarea#share-text').select();
+			updateURL( shortURL!==undefined?shortURL:longURL );
+			$( 'textarea#share-text' ).select();
 
-      if ( shortURL===undefined )
-        createShortURL( longURL );
-     });
+			if ( shortURL===undefined )
+				createShortURL( longURL );
+		 } );
 
-    $('textarea#share-text').focus(function(){
-      $(this).select();
-    });
+		$( 'textarea#share-text' ).focus( function(){
+			$( this ).select();
+		} );
 
-    $editorEl.find('input[type="reset"]').on('click', function(e) {
-      e.preventDefault();
-      $editorEl.hide();
-    });
+		$editorEl.find( 'input[type="reset"]' ).on( 'click', function( e ) {
+			e.preventDefault();
+			$editorEl.hide();
+		} );
 
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId            : facebookAppId,
-        autoLogAppEvents : true,
-        xfbml            : true,
-        version          : 'v2.10'
-      });
-      FB.AppEvents.logPageView();
-    };
+		window.fbAsyncInit = function() {
+			FB.init( {
+				appId						: facebookAppId,
+				autoLogAppEvents : true,
+				xfbml						: true,
+				version					: 'v2.10'
+			} );
+			FB.AppEvents.logPageView();
+		};
 
-    (function(d, s, id){
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) {return;}
-       js = d.createElement(s); js.id = id;
-       js.src = "//connect.facebook.net/en_US/sdk.js";
-       fjs.parentNode.insertBefore(js, fjs);
-     })(document, 'script', 'facebook-jssdk');
+		(function(d, s, id){
+			 var js, fjs = d.getElementsByTagName(s)[0];
+			 if (d.getElementById(id)) {return;}
+			 js = d.createElement(s); js.id = id;
+			 js.src = "//connect.facebook.net/en_US/sdk.js";
+			 fjs.parentNode.insertBefore(js, fjs);
+		 })( document, 'script', 'facebook-jssdk' );
 
-    $('#share-facebook a').on('click', function(e) {
-      e.preventDefault();
-      
-      FB.ui({
-      method: 'share',
-      href: longURL,
-    }, function(response){});
-    });
-  }
+		$( '#share-facebook a' ).on( 'click', function( e ) {
+			e.preventDefault();
 
-  function updateURL( url ) {
-    $('textarea#share-text').html(url).select();
+			FB.ui( {
+			method: 'share',
+			href: longURL,
+		}, function( response ) { } );
+		} );
+	}
 
-    var tweet = mw.config.get( 'wgPageName' ).replace( /_/g, ' ' ) + ' ' + url + ' #페미위키';
+	function updateURL( url ) {
+		$( 'textarea#share-text' ).html( url ).select();
 
-    $('#share-twitter a').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent( tweet ) );
-  }
+		var tweet = mw.config.get( 'wgPageName' ).replace( /_/g, ' ' ) + ' ' + url + ' #페미위키';
 
-  function createShortURL( longURL ) {
-    shortURL = undefined;
+		$( '#share-twitter a' ).attr( 'href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent( tweet ) );
+	}
 
-    var params = {
-    "dynamicLinkInfo": {
-      "dynamicLinkDomain": "fmwk.page.link",
-      "link": longURL,
-      "analyticsInfo": {
-        "googlePlayAnalytics": {
-          "utmCampaign": "share"
-        }
-      }
-    },
-    "suffix": {
-      "option": "SHORT"
-    }
-  };
+	function createShortURL( longURL ) {
+		shortURL = undefined;
 
-  $.ajax({
-    url: 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=' + firebaseKey,
-    type: 'POST',
-    data: JSON.stringify(params),
-    contentType: "application/json",
-    success: function (response) {
-        if ( longURL != window.location.href )
-          return;
-        shortURL = response.shortLink;
-        updateURL(shortURL);
-    },
-    error: function (request, status, error) {
-        console.log(request.responseText);
-    }
-});     
-  }
+		var params = {
+		"dynamicLinkInfo": {
+			"dynamicLinkDomain": "fmwk.page.link",
+			"link": longURL,
+			"analyticsInfo": {
+				"googlePlayAnalytics": {
+					"utmCampaign": "share"
+				}
+			}
+		},
+		"suffix": {
+			"option": "SHORT"
+		}
+	};
 
-  // Init UI
-  $(function() {
-    _initUI();
-  });
-})();
+	$.ajax( {
+		url: 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=' + firebaseKey,
+		type: 'POST',
+		data: JSON.stringify( params ),
+		contentType: "application/json",
+		success: function ( response ) {
+				if ( longURL != window.location.href )
+					return;
+				shortURL = response.shortLink;
+				updateURL( shortURL );
+		},
+		error: function ( request, status, error ) {
+				console.log( request.responseText );
+		}
+} );
+	}
 
+	// Init UI
+	$( function() {
+		_initUI();
+	} );
+} )();
