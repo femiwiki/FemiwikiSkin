096b544afa8e19d61ade5bf9ce09b9127b232dae
Hyeon Kim <simnalamburt@gmail.com>
2019-02-06 05:27:18 +0900

    Get rid of grunt-contrib-jshint, grunt-jsonlint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,12 @@
 /*jshint node:true */
 module.exports = function(grunt) {
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-jsonlint");
   grunt.loadNpmTasks("grunt-banana-checker");
 
   grunt.initConfig({
-    jshint: {
-      all: ["**/*.js", "!node_modules/**", "!vendor/**"]
-    },
     banana: {
       all: "i18n/"
-    },
-    jsonlint: {
-      all: ["**/*.json", "!node_modules/**", "!vendor/**"]
     }
   });
 
-  grunt.registerTask("default", ["jshint", "jsonlint", "banana"]);
+  grunt.registerTask("default", ["banana"]);
 };
