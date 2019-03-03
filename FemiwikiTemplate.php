37c5f9a47f6efda8b87b1a96f9d5aa0d1e06dc8e
lens0021 <lorentz0021@gmail.com>
2019-03-03 18:35:56 +0900

    Prepare L10n

--- FemiwikiTemplate.php
+++ FemiwikiTemplate.php
@@ -34,8 +34,8 @@ class FemiwikiTemplate extends BaseTemplate {
 					</button>
 
 					<ul id="site-navigation">
-						<li class="changes"><a href="/w/Special:RecentChanges" title="바뀐글"><span class="text">바뀐글</span></a></li>
-						<li class="random"><a href="/w/Special:RandomPage" title="임의글"><span class="text">임의글</span></a></li>
+						<li class="changes"><a href="/w/Special:RecentChanges" title="<?php echo $this->getMsg( 'tooltip-n-recentchanges' )->text() ?>"><span class="text"><?php echo $this->getMsg( 'recentchanges' )->text() ?></span></a></li>
+						<li class="random"><a href="/w/Special:RandomPage" title="<?php echo $this->getMsg( 'tooltip-n-randompage' )->text() ?>"><span class="text"><?php echo $this->getMsg( 'randompage' )->text() ?></span></a></li>
 					</ul>
 
 					<?php
