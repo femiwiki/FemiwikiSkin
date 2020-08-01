3d2ff14b8a7902e9f38890f1c017d1135efe22dc
lens0021 <lorentz0021@gmail.com>
2020-08-01 17:00:07 +0900

    Drop code for compatibility of 1.34

--- includes/FemiwikiResourceLoaderModule.php
+++ includes/FemiwikiResourceLoaderModule.php
@@ -7,26 +7,13 @@ class FemiwikiResourceLoaderModule extends \ResourceLoaderSkinModule {
 	 */
 	protected function getLessVars( \ResourceLoaderContext $context ) {
 		$lessVars = parent::getLessVars( $context );
-		$logosIsSupported = version_compare( MW_VERSION, '1.35', '>=' );
-
-		$logos = $this->getConfig()->get( $logosIsSupported ? 'Logos' : 'FemiwikiLogos' );
+		$logos = $this->getConfig()->get( 'Logos' );
 
 		if ( isset( $logos[ 'icon' ] ) ) {
 			$lessVars[ 'logo-icon-enabled'] = true;
 			$lessVars[ 'logo-icon-url' ] = CSSMin::buildUrlValue( $logos['icon'] );
 		} else {
 			$lessVars[ 'logo-icon-enabled'] = false;
-		}
-		if ( !$logosIsSupported ) {
-			if ( isset( $logos['wordmark'] ) ) {
-				$logo = $logos['wordmark'];
-				$lessVars[ 'logo-enabled' ] = true;
-				$lessVars[ 'logo-wordmark-url' ] = CSSMin::buildUrlValue( $logo['src'] );
-				$lessVars[ 'logo-wordmark-width' ] = intval( $logo['width'] );
-				$lessVars[ 'logo-wordmark-height' ] = intval( $logo['height'] );
-			} else {
-				$lessVars[ 'logo-enabled' ] = false;
-			}
 		}
 
 		return $lessVars;
