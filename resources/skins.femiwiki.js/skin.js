f6e23481ea816775d15d26f4864ee60650bf7cb7
lens0021 <lorentz0021@gmail.com>
2020-08-01 16:55:54 +0900

    Apply CheckboxHack
    Closes https://github.com/femiwiki/FemiwikiSkin/issues/156

--- resources/skins.femiwiki.js/skin.js
+++ resources/skins.femiwiki.js/skin.js
@@ -1,9 +1,38 @@
+/** @type {CheckboxHack} */ var checkboxHack = require(/** @type {string} */ ('mediawiki.page.ready'))
+  .checkboxHack;
+
+/**
+ * Improve the interactivity of the sidebar panel by binding optional checkbox hack enhancements
+ * for focus and `aria-expanded`. Also, flip the icon image on click.
+ *
+ * @param {HTMLElement|null} checkbox
+ * @param {HTMLElement|null} button
+ * @return {void}
+ */
+function initCheckboxHack(checkbox, button) {
+  if (checkbox instanceof HTMLInputElement && button) {
+    checkboxHack.bindToggleOnClick(checkbox, button);
+    checkboxHack.bindUpdateAriaExpandedOnInput(checkbox, button);
+    checkboxHack.updateAriaExpanded(checkbox, button);
+    checkboxHack.bindToggleOnSpaceEnter(checkbox, button);
+  }
+}
+
 /**
  * @return {void}
  */
 function main() {
   require('./searchClearButton.js').init();
   require('./notificationBadge.js').init();
+
+  initCheckboxHack(
+    window.document.getElementById('fw-menu-checkbox'),
+    window.document.getElementById('fw-menu-toggle')
+  );
+  initCheckboxHack(
+    window.document.getElementById('fw-page-menu-checkbox'),
+    window.document.getElementById('p-menu-toggle')
+  );
 
   var fwMenuToggle = document.querySelector('#fw-menu-toggle');
   var fwNotificationBadge = document.querySelector('#fw-menu-toggle .badge');
