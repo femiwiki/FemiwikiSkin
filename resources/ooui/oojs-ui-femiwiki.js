4b3268567a872efee2b23de6e874042ba3eaa533
lens0021 <lorentz0021@gmail.com>
2020-08-02 11:08:53 +0900

    Bump OOUI to v0.39.3

--- resources/ooui/oojs-ui-femiwiki.js
+++ resources/ooui/oojs-ui-femiwiki.js
@@ -1,114 +1,123 @@
 /*!
- * OOUI v0.30.1
+ * OOUI v0.39.3
  * https://www.mediawiki.org/wiki/OOUI
  *
- * Copyright 2011–2019 OOUI Team and other contributors.
+ * Copyright 2011–2020 OOUI Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2019-01-22T09:48:04Z
+ * Date: 2020-08-02T01:50:23Z
  */
-(function (OO) {
-  'use strict';
+( function ( OO ) {
 
-  /**
-   * @class
-   * @extends OO.ui.Theme
-   *
-   * @constructor
-   */
-  OO.ui.FemiwikiTheme = function OoUiWikimediaUITheme() {
-    // Parent constructor
-    OO.ui.FemiwikiTheme.parent.call(this);
-  };
+'use strict';
 
-  /* Setup */
+/**
+ * @class
+ * @extends OO.ui.Theme
+ *
+ * @constructor
+ */
+OO.ui.FemiwikiTheme = function OoUiWikimediaUITheme() {
+	// Parent constructor
+	OO.ui.FemiwikiTheme.super.call( this );
+};
 
-  OO.inheritClass(OO.ui.FemiwikiTheme, OO.ui.Theme);
+/* Setup */
 
-  /* Methods */
+OO.inheritClass( OO.ui.FemiwikiTheme, OO.ui.Theme );
 
-  /**
-   * @inheritdoc
-   */
-  OO.ui.FemiwikiTheme.prototype.getElementClasses = function (element) {
-    // Parent method
-    var variant,
-      isFramed,
-      isActive,
-      isToolOrGroup,
-      variants = {
-        warning: false,
-        invert: false,
-        progressive: false,
-        destructive: false,
-      },
-      // Parent method
-      classes = OO.ui.FemiwikiTheme.parent.prototype.getElementClasses.call(
-        this,
-        element
-      );
+/* Methods */
 
-    if (
-      element instanceof OO.ui.IconWidget &&
-      element.$element.hasClass('oo-ui-checkboxInputWidget-checkIcon')
-    ) {
-      // Icon on CheckboxInputWidget
-      variants.invert = true;
-    } else if (element.supports(['hasFlag'])) {
-      isFramed = element.supports(['isFramed']) && element.isFramed();
-      isActive = element.supports(['isActive']) && element.isActive();
-      isToolOrGroup =
-        // Check if the class exists, as classes that are not in the 'core' module may not be loaded
-        (OO.ui.Tool && element instanceof OO.ui.Tool) ||
-        (OO.ui.ToolGroup && element instanceof OO.ui.ToolGroup);
-      if (
-        // Button with a dark background
-        (isFramed &&
-          (isActive || element.isDisabled() || element.hasFlag('primary'))) ||
-        // Toolbar with a dark background
-        (isToolOrGroup && element.hasFlag('primary'))
-      ) {
-        // … use white icon / indicator, regardless of other flags
-        variants.invert = true;
-      } else if (!isFramed && element.isDisabled()) {
-        // Frameless disabled button, always use black icon / indicator regardless of other flags
-        variants.invert = false;
-      } else if (!element.isDisabled()) {
-        // Any other kind of button, use the right colored icon / indicator if available
-        variants.progressive =
-          element.hasFlag('progressive') ||
-          // Active tools/toolgroups
-          (isToolOrGroup && isActive) ||
-          // Pressed or selected outline/menu option widgets
-          ((element instanceof OO.ui.MenuOptionWidget ||
-            // Check if the class exists, as classes that are not in the 'core' module may not be loaded
-            (OO.ui.OutlineOptionWidget &&
-              element instanceof OO.ui.OutlineOptionWidget)) &&
-            (element.isPressed() || element.isSelected()));
+/**
+ * @inheritdoc
+ */
+OO.ui.FemiwikiTheme.prototype.getElementClasses = function ( element ) {
+	// Parent method
+	var variant, isFramed, isActive, isToolOrGroup,
+		variants = {
+			invert: false,
+			progressive: false,
+			destructive: false,
+			error: false,
+			warning: false,
+			success: false
+		},
+		// Parent method
+		classes = OO.ui.FemiwikiTheme.super.prototype.getElementClasses.call( this, element );
 
-        variants.destructive = element.hasFlag('destructive');
-        variants.warning = element.hasFlag('warning');
-      }
-    }
+	if (
+		element instanceof OO.ui.IconWidget &&
+		// eslint-disable-next-line no-jquery/no-class-state
+		element.$element.hasClass( 'oo-ui-checkboxInputWidget-checkIcon' )
+	) {
+		// Icon on CheckboxInputWidget
+		variants.invert = true;
+	} else if ( element.supports( [ 'hasFlag' ] ) ) {
+		isFramed = element.supports( [ 'isFramed' ] ) && element.isFramed();
+		isActive = element.supports( [ 'isActive' ] ) && element.isActive();
+		isToolOrGroup =
+			// Check if the class exists, as classes that are not in the 'core' module may
+			// not be loaded.
+			( OO.ui.Tool && element instanceof OO.ui.Tool ) ||
+			( OO.ui.ToolGroup && element instanceof OO.ui.ToolGroup );
+		if (
+			// Button with a dark background.
+			isFramed && ( isActive || element.isDisabled() || element.hasFlag( 'primary' ) ) ||
+			// Toolbar with a dark background.
+			isToolOrGroup && element.hasFlag( 'primary' )
+		) {
+			// … use white icon / indicator, regardless of other flags
+			variants.invert = true;
+		} else if ( !isFramed && element.isDisabled() && !element.hasFlag( 'invert' ) ) {
+			// Frameless disabled button, always use black icon / indicator regardless of
+			// other flags.
+			variants.invert = false;
+		} else if ( !element.isDisabled() ) {
+			// Any other kind of button, use the right colored icon / indicator if available.
+			variants.progressive = element.hasFlag( 'progressive' ) ||
+				// Active tools/toolgroups
+				( isToolOrGroup && isActive ) ||
+				// Pressed or selected outline/menu option widgets
+				(
+					(
+						element instanceof OO.ui.MenuOptionWidget ||
+						// Check if the class exists, as classes that are not in the 'core' module
+						// may not be loaded.
+						(
+							OO.ui.OutlineOptionWidget &&
+							element instanceof OO.ui.OutlineOptionWidget
+						)
+					) &&
+					( element.isPressed() || element.isSelected() )
+				);
 
-    for (variant in variants) {
-      classes[variants[variant] ? 'on' : 'off'].push('oo-ui-image-' + variant);
-    }
+			variants.destructive = element.hasFlag( 'destructive' );
+			variants.invert = element.hasFlag( 'invert' );
+			variants.error = element.hasFlag( 'error' );
+			variants.warning = element.hasFlag( 'warning' );
+			variants.success = element.hasFlag( 'success' );
+		}
+	}
 
-    return classes;
-  };
+	for ( variant in variants ) {
+		classes[ variants[ variant ] ? 'on' : 'off' ].push( 'oo-ui-image-' + variant );
+	}
 
-  /**
-   * @inheritdoc
-   */
-  OO.ui.FemiwikiTheme.prototype.getDialogTransitionDuration = function () {
-    return 250;
-  };
+	return classes;
+};
 
-  /* Instantiation */
+/**
+ * @inheritdoc
+ */
+OO.ui.FemiwikiTheme.prototype.getDialogTransitionDuration = function () {
+	return 250;
+};
 
-  OO.ui.theme = new OO.ui.FemiwikiTheme();
-})(OO);
+/* Instantiation */
+
+OO.ui.theme = new OO.ui.FemiwikiTheme();
+
+}( OO ) );
 
 //# sourceMappingURL=oojs-ui-femiwiki.js.map.json
