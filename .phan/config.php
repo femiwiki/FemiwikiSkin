2de3e60d1001d2e45dc9352cf7a8fc09eac417fd
lens0021 <lorentz0021@gmail.com>
2020-12-30 09:38:59 +0900

    Quibble and Phan

---
+++ .phan/config.php
@@ -0,0 +1,33 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/CentralAuth',
+		'../../extensions/Echo',
+		'../../extensions/EventLogging',
+		'../../extensions/EventStreamConfig',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/CentralAuth',
+		'../../extensions/Echo',
+		'../../extensions/EventLogging',
+		'../../extensions/EventStreamConfig',
+	]
+);
+
+$cfg['exclude_file_list'] = array_merge(
+	$cfg['exclude_file_list'],
+	[
+		// auto-generated
+		'includes/FemiwikiTheme.php',
+	]
+);
+
+return $cfg;
