3262f4b5a8372f08e7044475454728ebf71c6f56
lens0021 <lorentz0021@gmail.com>
2021-08-14 19:26:07 +0900

    Add a null check

--- includes/SmallElementsHooks.php
+++ includes/SmallElementsHooks.php
@@ -46,6 +46,7 @@ class SmallElementsHooks implements
 			( !$registered && $config->get( Constants::CONFIG_KEY_SMALL_ELEMENTS_FOR_ANONYMOUS_USER ) )
 			|| ( $registered && $userOptionsLookup->getOption( $user, Constants::PREF_KEY_LARGER_ELEMENTS, '0' ) === '0' )
 			) {
+			$bodyAttrs['class'] = $bodyAttrs['class'] ?? '';
 			$bodyAttrs['class'] .= ' fw-legacy-small-elements';
 		}
 	}
