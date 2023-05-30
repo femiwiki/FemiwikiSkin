3dc4996dc717412e3d0d9c9151519cf4aae85f41
Lens0021 / Leslie <lorentz0021@gmail.com>
2023-05-30 22:14:38 +0900

    Replace AddThis with navigator.share (#716)

--- resources/skins.femiwiki.share/init.js
+++ resources/skins.femiwiki.share/init.js
@@ -3,49 +3,36 @@
 
   function init() {
     OO.ui.infuse($('#p-share')).on('click', function () {
-      var windowManager, shareDialog;
-      var firebaseKey = firebaseKey || mw.config.get('wgFemiwikiFirebaseKey');
-      var useAddThis = useAddThis || mw.config.get('wgFemiwikiUseAddThis');
-      var addThisToolId =
-        addThisToolId || mw.config.get('wgFemiwikiAddThisToolId');
-
-      if (useAddThis) {
-        addthis_config = addthis_config || {};
-        addthis_config['services_exclude'] = 'print';
-        addthis_config['ui_language'] = mw.config.get('wgUserLanguage');
-        addthis_share = addthis_share || {};
-        addthis_share = {
-          passthrough: {
-            twitter: {
-              text: mw.config.get('wgPageName').replace(/_/g, ' '),
-              hashtags: mw.config.get('wgSiteName'),
-            },
-          },
-        };
-      }
-
-      mw.loader.using(['skins.femiwiki.share.ui']).done(function () {
-        windowManager = windowManager || OO.ui.getWindowManager();
-        if (shareDialog === undefined) {
-          shareDialog = new mw.fw.ShareDialog({
-            useAddThis: useAddThis,
-            addThisToolId: addThisToolId,
-            firebaseKey: firebaseKey,
-          });
-          windowManager.addWindows([shareDialog]);
-        }
-
-        windowManager.openWindow(shareDialog, {
+      if (navigator.share) {
+        navigator.share({
           url: window.location.href,
-          actions: [
-            {
-              action: 'accept',
-              label: mw.msg('skin-femiwiki-share-dismiss'),
-              flags: 'primary',
-            },
-          ],
+          title: mw.config.get('wgPageName').replace(/_/g, ' '),
         });
-      });
+      } else {
+        var windowManager, shareDialog;
+        var firebaseKey = firebaseKey || mw.config.get('wgFemiwikiFirebaseKey');
+
+        mw.loader.using(['skins.femiwiki.share.ui']).done(function () {
+          windowManager = windowManager || OO.ui.getWindowManager();
+          if (shareDialog === undefined) {
+            shareDialog = new mw.fw.ShareDialog({
+              firebaseKey: firebaseKey,
+            });
+            windowManager.addWindows([shareDialog]);
+          }
+
+          windowManager.openWindow(shareDialog, {
+            url: window.location.href,
+            actions: [
+              {
+                action: 'accept',
+                label: mw.msg('skin-femiwiki-share-dismiss'),
+                flags: 'primary',
+              },
+            ],
+          });
+        });
+      }
     });
   }
 
